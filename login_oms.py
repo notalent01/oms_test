453dee8f84ddba75d573e24eaa0aea9da3cfc6d3
chriswangs <chriswangs@pptv.com>
2017-08-17 09:49:20 +0800

    1.获取cookie采用第三方js模拟取代chrome webdriver的方式（不需要启动浏览器）

--- login_oms.py
+++ login_oms.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
-# def get_loginCookies(uname, pwd):
 def get_loginCookies():
-    driver = webdriver.Chrome()
+    from selenium import webdriver
+    path = r'C:\Users\chriswangs\AppData\Local\Programs\Python\Python35-32\Scripts\phantomjs.exe'
+    driver = webdriver.PhantomJS(executable_path=path)
     url_login = "http://oms.synacast.com"
     driver.get(url_login)
-    driver.find_element_by_xpath("/html/body/div[1]/div/ul/li[2]").click()
-    driver.find_element_by_xpath("//*[@id=\"domain\"]/a").click()
-    driver.find_element_by_id("username").clear()
-    driver.find_element_by_id("username").send_keys("chriswangs")
-    driver.find_element_by_id("password").send_keys("Wswswps02")
+    driver.find_element_by_xpath('/html/body/div[1]/div/ul/li[1]').click()
+    driver.find_element_by_xpath('//*[@id="douya"]/a').click()
+    driver.find_element_by_id("username").send_keys("16091955")
+    driver.find_element_by_id("password").send_keys("Talent03")
     driver.find_element_by_name("submit").click()
     print(driver.get_cookies())
     cookie = [item["name"] + "=" + item["value"] for item in driver.get_cookies()]
